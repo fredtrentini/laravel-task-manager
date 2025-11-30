92601c3c7831753e18d66adb86b428b9703d279b
Fred <fredtrentini@gmail.com>
2025-11-30 14:50:47 -0300

    Remove unused features

--- laravel-task-manager/app/Models/User.php
+++ laravel-task-manager/app/Models/User.php
@@ -5,12 +5,11 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Laravel\Fortify\TwoFactorAuthenticatable;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class User extends Authenticatable
 {
-    use HasFactory, Notifiable, TwoFactorAuthenticatable;
+    use HasFactory, Notifiable;
 
     protected $fillable = [
         'name',
@@ -20,15 +19,11 @@ class User extends Authenticatable
 
     protected $hidden = [
         'password',
-        'two_factor_secret',
-        'two_factor_recovery_codes',
         'remember_token',
     ];
 
     protected $casts = [
-        'email_verified_at' => 'datetime',
         'password' => 'hashed',
-        'two_factor_confirmed_at' => 'datetime',
     ];
 
     public function projects(): HasMany
